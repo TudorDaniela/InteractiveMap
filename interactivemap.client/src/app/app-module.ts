6feeb57543bc2a4c0dd20a6dc220aeb338fdadcc
TudorDaniela <danielatudor95@yahoo.com>
2026-02-21 22:23:28 +0200

    Added the world-map component

--- interactivemap.client/src/app/app-module.ts
+++ interactivemap.client/src/app/app-module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { WorldMapComponent } from './components/world-map/world-map.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    WorldMapComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
---
+++ interactivemap.client/src/app/components/world-map/world-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-world-map',
+  standalone: false,
+  templateUrl: './world-map.html',
+  styleUrl: './world-map.css',
+})
+export class WorldMapComponent {
+
+}
